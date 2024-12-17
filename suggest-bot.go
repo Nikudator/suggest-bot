4056ee5f64a00889a524926e92c7075d2c508473
Николай Алексеевич Куропятников <Kuropjatnikov@mtr.loc>
2024-12-17 15:11:48 +0300

    Init

---
+++ suggest-bot.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+    "log"
+   // "os"
+    tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+func main() {
+    bot, err := tgbotapi.NewBotAPI("7208891763:AAGvBxeFxqLKtmXvDsJ7NDYPJb6s6Oc8YAY")
+    // 
+    if err != nil {
+        log.Panic(err)
+    }
+
+    bot.Debug = true
+
+    log.Printf("Authorized on account %s", bot.Self.UserName)
+
+    u := tgbotapi.NewUpdate(0)
+    u.Timeout = 60
+
+    updates := bot.GetUpdatesChan(u)
+
+for update := range updates {
+    if update.Message != nil {
+        log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+        var msg tgbotapi.MessageConfig
+
+        switch update.Message.Command() {
+        case "start":
+            msg = tgbotapi.NewMessage(update.Message.Chat.ID, "Welcome! I am your bot.")
+        case "help":
+            msg = tgbotapi.NewMessage(update.Message.Chat.ID, "I can help you with the following commands:\n/start - Start the bot\n/help - Display this help message")
+        default:
+            msg = tgbotapi.NewMessage(update.Message.Chat.ID, "I don't know that command")
+        }
+
+        bot.Send(msg)
+    }
+}
+}
