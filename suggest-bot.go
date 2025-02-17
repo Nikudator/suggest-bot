1e177f8a3848788435a91a0834dc80cba5606c8a
Nikolai A. Kuropyatnikov <nickolyan@google.com>
2025-02-17 13:35:04 +0300

    Start message

--- suggest-bot.go
+++ suggest-bot.go
@@ -57,7 +57,7 @@ func main() {
 
 			switch update.Message.Command() {
 			case "start":
-				msg = tgbotapi.NewMessage(update.Message.Chat.ID, "Приветствую! Я бот для отправки сообщений и постов на канал <a href=\"t.me/real_shush\">Реальное Шушенское</a> \n Просто напишите сообщение мне и я передам его администратору")
+				msg = tgbotapi.NewMessage(update.Message.Chat.ID, "Приветствую! Я бот для отправки сообщений и постов на канал \"Реальное Шушенское\" t.me/real_shush \n Просто напишите сообщение мне и я передам его администратору")
 			case "help":
 				msg = tgbotapi.NewMessage(update.Message.Chat.ID, "Я поддерживаю следующие комманды:\n/start - Старт бота\n /help - Показать помощь\n Если хотите опубликовать пост или написать администратору сообщение, просто напишите его и, если нужно, прикрепите фото или видео.")
 			default:
